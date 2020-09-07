4d4d339f0fad784b83434e0ad2112b1941af08f5
Evgeny Cherkasov <echerkasov-ki16@stud.sfu-kras.ru>
2020-09-07 15:03:26 +0700

    Initial Commit

---
+++ Swifty Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty Chat
+//
+//  Created by Евгений Черкасов on 07.09.2020.
+//  Copyright © 2020 Евгений Черкасов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
